c8d913385028cd7e9256d53f3e66db76c5de7c01
ravikoppula <ravikoppula92@gmail.com>
2023-05-12 12:02:16 +0800

    initial commit empty sol

---
+++ NetCore7.DownloadExcel.OpenXML_and_iText7/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NetCore7.DownloadExcel.OpenXML_and_iText7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
